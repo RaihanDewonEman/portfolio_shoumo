a6ba1f3a7e913a98ce3d596cf75f3eebfde36f21
RaihanDewonEman <raihan11011997@gmail.com>
2021-09-10 11:28:53 +0600

    First Commit

---
+++ myinfo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def Homepage(request):
+    return render(request, 'myinfo/home.html', {'Name': 'Home Page'})
